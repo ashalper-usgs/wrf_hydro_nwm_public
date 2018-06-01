07e469db87e7c45b4596f7730616fad436ed5810
Andrew Halper <ashalper>
2018-06-01 14:01:39 -0400

    Removed duplicates from Routing_objects and sorted remainder.

--- trunk/NDHMS/global.mk
+++ trunk/NDHMS/global.mk
@@ -7,20 +7,19 @@ Data_Rec_objects = $(addsuffix .o,\
 )
 
 Routing_objects = $(addsuffix .o,\
-   module_date_utilities_rt \
-   module_UDMAP \
-   module_HYDRO_utils \
-   module_noah_chan_param_init_rt \
-   module_GW_baseflow \
-   module_gw_gw2d \
-   module_HYDRO_io \
-   module_RT \
    Noah_distr_routing \
-   module_channel_routing \
-   module_lsm_forcing \
-   module_date_utilities_rt \
-   module_NWM_io_dict \
+   module_GW_baseflow \
+   module_HYDRO_io \
+   module_HYDRO_utils \
    module_NWM_io \
+   module_NWM_io_dict \
+   module_RT \
+   module_UDMAP \
+   module_channel_routing \
+   module_date_utilities_rt \
+   module_gw_gw2d \
+   module_lsm_forcing \
+   module_noah_chan_param_init_rt \
 )
 
 HYDRO_drv_objects = $(addsuffix .o,module_HYDRO_drv)
