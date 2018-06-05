81d402cd7c0f827d38c45bd2e9e0372a0f9bff17
Andrew Halper <ashalper>
2018-06-05 16:17:03 -0400

    Added conditional to set WRF_HYDRO_NUDGING.

--- trunk/NDHMS/global.mk
+++ trunk/NDHMS/global.mk
@@ -1,6 +1,12 @@
-# make some conditional syntax easier to read
+# make some Makefile conditional syntax easier to read
 ifeq ($(findstring -DWRF_HYDRO_RAPID=1,$(DEFS)),"-DWRF_HYDRO_RAPID=1")
   WRF_HYDRO_RAPID = 1
 else
   WRF_HYDRO_RAPID = 0
+endif
+
+ifeq ($(findstring -DWRF_HYDRO_NUDGING=1,$(DEFS)),"-DWRF_HYDRO_NUDGING=1")
+  WRF_HYDRO_NUDGING = 1
+else
+  WRF_HYDRO_NUDGING = 0
 endif
